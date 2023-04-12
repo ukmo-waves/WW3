45754ab69cc89c40c13439be9cd8dc887fc793be
lsampson <lewis.sampson@metoffice.gov.uk>
2023-04-12 13:54:04 +0000

    Cleaned up miscellaneous code changes before final PR.

--- model/src/w3idatmd.F90
+++ model/src/w3idatmd.F90
@@ -266,9 +266,6 @@ MODULE W3IDATMD
   LOGICAL, POINTER        ::  FLLEVTIDE, FLCURTIDE,  &
        FLLEVRESI, FLCURRESI
 #endif
-#ifdef W3_GPU
-!$ACC DECLARE COPYIN(INFLAGS2, ICEP2)
-#endif
   !/
 CONTAINS
   !/ ------------------------------------------------------------------- /
